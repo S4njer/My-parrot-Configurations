37ffebd4247523e50f0f5042ea17812c31e613ba
S4njer <125299897+S4njer@users.noreply.github.com>
2025-03-20 18:35:13 +0100

    Nvchad error test

--- autoInstaller.sh
+++ autoInstaller.sh
@@ -11,7 +11,12 @@ function help() {
 function nvchadInstall() {
   echo -e "[!] Installing nvchad...\n"
   wget https://github.com/neovim/neovim/releases/download/v0.10.0/nvim-linux64.tar.gz
+  tar x -f nvim-linux64.tar.gz
+  sudo cp -r nvim-linux64/* ~/.local/
   rm -rf nvim-linux64 nvim-linux64.tar.gz 2>/dev/null
+  if [[ -d "$HOME/.config/nvim" ]]; then
+    rm -rf $HOME/.config/nvim 2>/dev/null
+  fi
   git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
   echo -e "\n[?] Done"
 }
